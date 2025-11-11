ab7c0e6296afe038b0a631cffc8c38d2d37d43ce
Veranece <1440832899@qq.com>
2025-11-11 15:37:52 +0800

    feat: 简单测试下数据生成和api调用

---
+++ backend/generateData.py
@@ -0,0 +1,37 @@
+import os
+os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com' 
+
+from langchain_community.vectorstores import Chroma
+# from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from sentence_transformers import SentenceTransformer
+from langchain_community.document_loaders import Docx2txtLoader
+from langchain_text_splitters import SpacyTextSplitter
+
+
+loader = Docx2txtLoader()
+documents = loader.load("/home/liziwei/Emergency-LLM/backend/resource/规划.docx")
+
+
+model = HuggingFaceEmbeddings(
+    model_name="/New_Disk/liziwei/maidalun1020/bce-embedding-base_v1",
+    model_kwargs={"device": "cuda"},
+    encode_kwargs={"normalize_embeddings": True}
+)
+
+text_splitter = SpacyTextSplitter(
+    chunk_size=200,
+    chunk_overlap=50,
+    pipeline="zh_core_web_sm"
+)
+
+
+# 3. 创建向量数据库
+vdb = Chroma.from_documents(
+    documents=text_splitter.split_documents(documents),
+    embedding=model,
+    persist_directory="/home/liziwei/Emergency-LLM/backend/vdb"
+)
+
+vdb.persist()
+print("向量数据库创建成功！")
